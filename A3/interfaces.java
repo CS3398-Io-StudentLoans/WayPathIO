7420060e6b6cc073dcad0748f12191fb70a6181a
MaojunT <42899286+MaojunT@users.noreply.github.com>
2018-09-06 14:31:42 -0500

    Update interfaces.java
    added comments

--- A3/interfaces.java
+++ A3/interfaces.java
@@ -4,6 +4,8 @@ import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.io.*;
 
+//separating IWorker interface into 2 which implies the Interface Segregation Principle.
+//The robot class will not implement the eat method.
 interface IWorkable {
     public void work();
 }
