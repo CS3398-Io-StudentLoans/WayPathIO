f9086bb247ef9545c28bb3e6d56c3129a6b890f7
MaojunT <42899286+MaojunT@users.noreply.github.com>
2018-09-10 18:35:20 -0500

    Update worker.java

--- ThreeSolidMain/src/worker.java
+++ ThreeSolidMain/src/worker.java
@@ -4,9 +4,6 @@ import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.io.*;
 
-//import ThreeSolidPrinciples.interfaces;
-
-
 
 class Worker implements IWorkable, IFeedable{
     public void work() {
