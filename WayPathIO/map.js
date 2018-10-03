4e9f82bb3f9f6cf0cf28443e40d1d162569c8ac7
rolandomartinez63 <42542799+rolandomartinez63@users.noreply.github.com>
2018-10-02 23:43:41 -0500

    Added another button that can be used to navigate
    The E on the map will direct to some location. I'm going to try to figure out how to use that new button to navigate around the map.

--- WayPathIO/map.js
+++ WayPathIO/map.js
@@ -4,18 +4,28 @@ var txState = [-97.942999, 29.888998];
 var txStateWebMercator = ol.proj.fromLonLat(txState);
 
 var map = new ol.Map({
+    
+    controls: ol.control.defaults().extend([
+        new ol.control.ZoomToExtent({
+            extent: [
+                8179.7791264898, 5920020.284081122,
+        848966.963801, 5936863.986909639
+            ]
+        })
+    ]),
         target: 'map',
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
           })
         ],
-        
+
         view: new ol.View({
         center: txStateWebMercator,
         zoom: 16
         })
       });
+
 
 /*mapboxgl.accessToken = 'pk.eyJ1IjoibGVkYW5pZWxuIiwiYSI6ImNqbWxsZGQ0ZTA5amUzam55b3poaG0wN3UifQ.-6Mmg4GCbTFKybn_1i8dRg';
   
