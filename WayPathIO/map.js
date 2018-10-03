97afe9e378712763f639c90ac69385f2b8bb28f9
rolandomartinez63 <rim26@txstate.edu>
2018-10-03 13:59:02 -0500

    Added some items to drop down list. panning unsuccessful
    The Alkek button will make a duplicate of the current map instead of moving the map we already have.  In the map.js file, try to edit the "panAlk" function to make the alkek button do what it's supposed to.

--- WayPathIO/map.js
+++ WayPathIO/map.js
@@ -1,18 +1,15 @@
 //Follow this website for tutorial: https://openlayers.org/en/latest/doc/quickstart.html
-
 var txState = [-97.942999, 29.888998];
+var tmh = [-97.941480, 29.889306];
+var fh = [ -97.940402, 29.888836];
+var alk = [-97.943114, 29.888868];
+
+//var txStateWebMercator1 = ol.proj.fromLonLat(tmh);
+
 var txStateWebMercator = ol.proj.fromLonLat(txState);
 
 var map = new ol.Map({
     
-    controls: ol.control.defaults().extend([
-        new ol.control.ZoomToExtent({
-            extent: [
-                8179.7791264898, 5920020.284081122,
-        848966.963801, 5936863.986909639
-            ]
-        })
-    ]),
         target: 'map',
         layers: [
           new ol.layer.Tile({
@@ -23,8 +20,29 @@ var map = new ol.Map({
         view: new ol.View({
         center: txStateWebMercator,
         zoom: 16
+        }),
+    
+      });
+
+//function to control Alkek
+function panAlk(){
+var txStateWebMercator1 = ol.proj.fromLonLat(alk);
+        var map = new ol.Map({
+    
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+
+        view: new ol.View({
+        center: txStateWebMercator1,
+        zoom: 17
         })
       });
+
+};
 
 
 /*mapboxgl.accessToken = 'pk.eyJ1IjoibGVkYW5pZWxuIiwiYSI6ImNqbWxsZGQ0ZTA5amUzam55b3poaG0wN3UifQ.-6Mmg4GCbTFKybn_1i8dRg';
