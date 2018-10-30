cda9d21c43be14e8afe65d7978f0ca408fc165aa
Brian Chamblee <brc70@txstate.edu>
2018-10-30 05:41:18 -0500

    Node.js , express, pug, mongodb
    Look in this path nodejs>basic>myapp
    
    you must be running the npm start command and the mongod command for the server to be working and for the db to be running. I'm currently having connection issues which will be solved soon. But everything is set up for the most part. Also I have the web pages now running using the pug template which corresponds with ecpress and node.js .

---
+++ WayPathIO/node_js.js/basic/myapp/routes/buildingdata.js
@@ -0,0 +1,82 @@
+
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('buildingdata', { title: 'WayPathIO' });
+});
+
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('building-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('buildingdata', {items: resultArray});
+    });
+  });
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    buildingname: req.body.buildingname,
+    busylevel: req.body.busylevel,
+    time: req.body.time
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('building-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+router.post('/update', function(req, res, next) {
+  var item = {
+    buildingname: req.body.buildingname,
+    busylevel: req.body.busylevel,
+    time: req.body.time
+  };
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('building-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item updated');
+      db.close();
+    });
+  });
+});
+
+router.post('/delete', function(req, res, next) {
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('building-data').deleteOne({"_id": objectId(id)}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item deleted');
+      db.close();
+    });
+  });
+});
+
+module.exports = router;
+
+
